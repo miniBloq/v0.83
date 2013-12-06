f3def9a82c3aade0846e19ebd06c80b07ff2bad8
Julian da Silva <julian.dasilva.gillig@gmail.com>
2013-12-06 00:01:34 -0300

    Added getter for areMotorsRunning() function in Sparki.

---
+++ source/Bin/Minibloq/hard/DuinoBot.Kids.v1.x.HID/lib/board.h
@@ -0,0 +1,26 @@
+#ifndef board_h
+#define board_h
+
+#define serial0 Serial
+#define serial1 Serial1
+
+#define BuzzerPin 23
+
+#define sensor0  0
+#define sensor1  1
+#define sensor2  2
+#define sensor3  3
+#define sensor4  4
+#define TemperaturePin   7
+		
+#define D14 14
+#define D15 15
+#define D16 16
+#define D17 17
+#define D18 18
+#define D23_Buzzer 23
+#define D25_RunButton 25
+
+void initBoard();
+
+#endif
---
+++ source/Bin/Minibloq/work/_examples/Arduino_Seeed_DuinoBot_DuinoBot.Kids/230.PingMotor/PingMotor/PingMotor.cpp
@@ -0,0 +1,17 @@
+#include <mbq.h>
+#include <PingIRReceiver.h>
+
+void setup()
+{
+	initBoard();
+
+	while(true)
+	{
+		motor0.setPower(pingMeasureCM(D15));
+		delay(200);
+	}
+}
+
+void loop()
+{
+}
