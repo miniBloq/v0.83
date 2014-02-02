7fa71007df4584d4c2f91b0c81fc37c93d3c7fc7
Julian da Silva <julian.dasilva.gillig@gmail.com>
2014-02-01 21:34:12 -0300

    Some numeric blocks added. SimpleMaze example added.

--- source/Bin/Minibloq/work/_examples/miniSim/90.Maze/maze0/maze.py
+++
@@ -1,9 +0,0 @@
-def drawMaze(robot):
-	robot.penWidth = 30
-
-	robot.penUp()
-	robot.rotate(-90)
-	robot.move(300-robot.penWidth)
-	robot.rotate(90)
-	robot.penDown()
-	robot.move(400-robot.penWidth)
