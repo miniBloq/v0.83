f9f82986b7d40bbba3d851866b9a911db2fc5775
Julian da Silva <julian.dasilva.gillig@gmail.com>
2014-02-06 21:55:35 -0300

    Started to work to add the SparkFun RedBot Board. Basic functions already working!

---
+++ source/Bin/Minibloq/work/_examples/DuinoBot/30.CounterSerial/CounterSerial/CounterSerial.cpp
@@ -0,0 +1,18 @@
+#include <mbq.h>
+#include <PingIRReceiver.h>
+
+void setup()
+{
+	initBoard();
+	float counter = 0;
+	while(true)
+	{
+		serial0.println(counter);
+		counter = (counter+1);
+		delay(300);
+	}
+}
+
+void loop()
+{
+}
