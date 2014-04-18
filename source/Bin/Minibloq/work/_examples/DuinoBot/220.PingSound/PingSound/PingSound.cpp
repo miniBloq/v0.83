26b4bfaef8aa92f127856c0027b945d84f63e05e
Julian da Silva <julian.dasilva.gillig@gmail.com>
2014-04-18 14:01:32 -0700

    Started to work on the adding of the Pi-Bot educational robot.

---
+++ source/Bin/Minibloq/work/_examples/DuinoBot/220.PingSound/PingSound/PingSound.cpp
@@ -0,0 +1,15 @@
+#include <mbq.h>
+#include <PingIRReceiver.h>
+
+void setup()
+{
+	initBoard();
+	while(true)
+	{
+		toneWithDelay(BuzzerPin, (pingMeasureCM(D15)*400), 50);
+	}
+}
+
+void loop()
+{
+}
---
+++ source/Bin/Minibloq/work/_examples/DuinoBot/220.PingSound/PingSound/initBoard.cpp
@@ -0,0 +1,26 @@
+#include <mbq.h>
+
+
+DCMotor motor0(22, 20, 21);
+DCMotor motor1(3, 4, 8);
+Servo servo0;
+Servo servo1;
+IRrecv irReceiver(D14);
+PingSensor ping(0);
+IRRanger irRanger20to150(1, IRRanger::range20to150cm);
+IRRanger irRanger10to80(1, IRRanger::range10to80cm);
+
+
+void initBoard()
+{
+	Serial.begin(115200);
+	Serial1.begin(115200);
+
+	randomSeed(analogRead(0));
+
+	//This is for the Multiplo robots:
+	//motor1.setClockwise(false);
+	//setPin() does not attach the servo (so the pin can be used by other library if the servo is not used).
+	servo0.setPin(16);
+	servo1.setPin(17);
+}
