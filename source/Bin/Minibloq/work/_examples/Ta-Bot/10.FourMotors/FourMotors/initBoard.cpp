cd1e776cc1f5157e63933f68e3e19bde515d7826
Julian da Silva <julian.dasilva.gillig@gmail.com>
2014-05-02 14:52:10 -0700

    Ta-Bot uses now its four motors.

---
+++ source/Bin/Minibloq/work/_examples/Ta-Bot/10.FourMotors/FourMotors/initBoard.cpp
@@ -0,0 +1,31 @@
+#include <mbq.h>
+
+
+AF_DCMotor afMotor0(1, MOTOR12_1KHZ);
+AF_DCMotor afMotor1(2, MOTOR12_1KHZ);
+AF_DCMotor afMotor2(3);
+AF_DCMotor afMotor3(4);
+DCMotorWrap motor0(&afMotor0);
+DCMotorWrap motor1(&afMotor1);
+DCMotorWrap motor2(&afMotor2);
+DCMotorWrap motor3(&afMotor3);
+Servo servo0;
+Servo servo1;
+IRrecv irReceiver(14);
+HCSRSensor hcsr(0);
+PingSensor ping(0);
+IRRanger irRanger20to150(1, IRRanger::range20to150cm);
+IRRanger irRanger10to80(1, IRRanger::range10to80cm);
+
+
+void initBoard()
+{
+	Serial.begin(115200);
+
+	//Uses the analog input 0, that may have the same value in some designs, but it's not so prabably:
+	randomSeed(analogRead(0));
+
+	//setPin() does not attach the servo (so the pin can be used by other library if the servo is not used).
+	servo0.setPin(9);
+	servo1.setPin(10);
+}
---
+++ source/Bin/Minibloq/work/_examples/Ta-Bot/10.FourMotors/FourMotors/mbq.h
@@ -0,0 +1,37 @@
+#ifndef mbq_h
+#define mbq_h
+
+
+#include <Arduino.h>
+#include <pins_arduino.h>
+#include <stdlib.h>
+#include <miniBloq.h>
+#include <board.h>
+#include <pitches.h>
+#include <toneDelay.h>
+#include <DCMotorWrap.h>
+#include <AFMotor.h>
+#include <Servo.h>
+#include <IRRemote.h>
+#include <HCSR0x.h>
+#include <Ping.h>
+#include <IRRanger.h>
+
+
+extern AF_DCMotor afMotor0;
+extern AF_DCMotor afMotor1;
+extern AF_DCMotor afMotor2;
+extern AF_DCMotor afMotor3;
+extern DCMotorWrap motor0;
+extern DCMotorWrap motor1;
+extern DCMotorWrap motor2;
+extern DCMotorWrap motor3;
+extern Servo servo0;
+extern Servo servo1;
+extern IRrecv irReceiver;
+extern HCSRSensor hcsr;
+extern PingSensor ping;
+extern IRRanger irRanger20to150;
+extern IRRanger irRanger10to80;
+
+#endif
