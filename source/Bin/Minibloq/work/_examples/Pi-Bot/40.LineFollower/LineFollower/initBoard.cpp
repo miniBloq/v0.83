9b0598b913889ea70820c69a3e5e2202150b478d
Julian da Silva <julian.dasilva.gillig@gmail.com>
2014-05-02 16:59:41 -0700

    Bool2Number block added.

---
+++ source/Bin/Minibloq/work/_examples/Pi-Bot/40.LineFollower/LineFollower/initBoard.cpp
@@ -0,0 +1,25 @@
+#include <mbq.h>
+
+
+Mot8833 motor0(3, 5);
+Mot8833 motor1(6, 11, false);
+Servo servo0;
+Servo servo1;
+IRrecv irReceiver(4);
+HCSRSensor hcsr(0);
+PingSensor ping(0);
+IRRanger irRanger20to150(1, IRRanger::range20to150cm);
+IRRanger irRanger10to80(1, IRRanger::range10to80cm);
+
+
+void initBoard()
+{
+	Serial.begin(115200);
+
+	//Uses the analog input 0, that may have the same value in some designs, but it's not so prabably:
+	randomSeed(analogRead(0));
+
+	//setPin() does not attach the servo (so the pin can be used by other library if the servo is not used).
+	servo0.setPin(9);
+	servo1.setPin(10);
+}
---
+++ source/Bin/Minibloq/work/_examples/Pi-Bot/40.LineFollower/LineFollower/mbq.h
@@ -0,0 +1,30 @@
+#ifndef mbq_h
+#define mbq_h
+
+
+#include <Arduino.h>
+#include <pins_arduino.h>
+#include <stdlib.h>
+#include <miniBloq.h>
+#include <board.h>
+#include <pitches.h>
+#include <toneDelay.h>
+#include <Mot8833.h>
+#include <Servo.h>
+#include <IRRemote.h>
+#include <HCSR0x.h>
+#include <Ping.h>
+#include <IRRanger.h>
+
+
+extern Mot8833 motor0;
+extern Mot8833 motor1;
+extern Servo servo0;
+extern Servo servo1;
+extern IRrecv irReceiver;
+extern HCSRSensor hcsr;
+extern PingSensor ping;
+extern IRRanger irRanger20to150;
+extern IRRanger irRanger10to80;
+
+#endif
