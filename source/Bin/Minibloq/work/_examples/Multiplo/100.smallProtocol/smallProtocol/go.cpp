67e1b19e94edf1615a2a3b54698800e4e76749bf
Julian da Silva <julian.dasilva.gillig@gmail.com>
2014-05-06 13:47:37 -0700

    Multiplo N6 robot: added OpenCV + Bluetooth example.

---
+++ source/Bin/Minibloq/work/_examples/Multiplo/100.smallProtocol/smallProtocol/go.cpp
@@ -0,0 +1,193 @@
+#include <mbq.h>
+
+#define debug 0
+#if (debug)
+	//debug (USB serial):
+	#define baudrate 115200
+	#define serial serial0
+#else
+	//Multiplo robots (N6, N8, Ta-Bot...):
+	#define baudrate 9600
+	#define serial serial1
+#endif
+
+void stopAll()
+{
+  motor0.setPower(0);
+  motor1.setPower(0);
+  noTone(BuzzerPin);
+}
+
+
+String getCommand()
+{
+	int readByte = 0;
+	String command = "";
+
+	do
+	{
+		if (serial.available()) 
+		{
+			readByte = serial.read();
+			//serial.write(readByte); //Debug: echo on
+			command = command + (char)readByte;
+		}
+	} while ((readByte != 13) && (readByte != 10)); //Any kind of line end or CR char marks the end of the current command.
+
+	//Normalizes the command:
+	//String strResult(command.toLowerCase());
+	command.trim();
+	command.toLowerCase();
+	return command;
+}
+
+
+//Affects the command input string writing to it its own tail:
+bool getIntParam(int &result, String& command, int min, int max)
+{
+	command = command.trim();
+	String aux = command;
+	String number = "";
+	int i = 0;
+	int len = command.length();
+
+	//Get the sign:
+	if (i <= len)
+	{
+		if ( (command[i] == '-') || (command[i] == '+') )
+		{
+			number = number + command[i];
+			i++;
+		}
+	}
+
+	//Get the number itself:
+	while (i <= len)
+	{
+		if ( ('0' <= command[i]) && (command[i] <= '9') )
+			number = number + command[i];
+		else
+			break;   
+		i++;
+	}
+
+	//  if (i == 0)
+	//  {
+	//     //No number found: command and result keeps the same:
+	//     return false;
+	//  }
+
+	//Numerical result:
+	const int chrBufLen = 32;
+	char chrBuf[chrBufLen];
+	number.toCharArray(chrBuf, chrBufLen);
+	//  result = atoi(chrBuf);
+	long int tempResult = strtol(chrBuf, NULL, 10);
+	if (tempResult > max)
+		tempResult = max;
+	if (tempResult < min)
+		tempResult = min;
+	result = (int)tempResult;
+
+	//Tail result:
+	if (i == len)
+	{
+		command = "";
+	}
+	else
+	{
+		command = aux.substring(i); //Returns the command's tail
+		command = command.trim();
+	}
+
+	//Success!
+	return true;
+}
+
+
+void parseCommand(const String& command)
+{
+	String returnString = "";
+	String tail = "";
+	String opcode = command.substring(0, command.indexOf(" "));
+	if (opcode == "")
+		opcode = command.substring(0, command.indexOf("\n"));
+	if (opcode == "")
+		opcode = command.substring(0, command.indexOf("\r"));
+	tail = command.substring(command.indexOf(" "));
+	if (tail == "")
+		tail = command.substring(0, command.indexOf("\n"));
+	if (tail == "")
+		tail = command.substring(0, command.indexOf("\r"));
+
+	//tail.trim();
+
+	//Debug:
+	//serial.print("opcode = ");
+	//serial.println(opcode);
+	//serial.print("command = ");
+	//serial.println(tail);
+
+	//serial.println(opcode); //debug.
+	if (opcode.trim() == "motors")
+	{
+		//serial.println(opcode); //debug.
+		int power0 = 0, power1 = 0;
+		//serial.println(tail.trim()); //##debug.
+		if (getIntParam(power0, tail, -100, 100))
+		{
+			getIntParam(power1, tail, -100, 100);
+		}
+		motor0.setPower(power1);
+		motor1.setPower(power0);
+		returnString = "["+ opcode + " " + power0 + " " + power1 + " " + millis() + "]";
+		serial.println(returnString);
+	}
+	else if (opcode.trim() == "tone")
+	{
+		int freq = 0, duration = 0;
+		if (getIntParam(freq, tail, 0, 10000))
+		{
+			getIntParam(duration, tail, 0, 10000);
+		}
+		if (freq != 0)
+			tone(BuzzerPin, freq, duration);
+		else
+			noTone(BuzzerPin);
+		returnString = "["+ opcode + " " + freq + " " + duration + " " + millis() + "]";
+		serial.println(returnString);
+	}
+	else if (opcode.trim() == "stop")
+	{
+		stopAll();
+		returnString = "["+ opcode + " " + millis() + "]";
+		serial.println(returnString);
+	}
+	else if (opcode == "")
+	{
+	}
+}
+
+
+void go()
+{
+	String command = "";
+	serial.begin(baudrate);
+
+	motor1.setClockwise(false);
+
+	//Tells the user that the interpreter is running:  
+	tone(BuzzerPin, 440, 300);
+	delay(300);
+	tone(BuzzerPin, 880, 500);
+
+	//serial.println("Robot protocol v0.1");
+
+	//Main application cycle (more eficiente than using the loop function):
+	while(true)
+	{
+		command = getCommand();
+	    //serial.println(command); //##debug.
+		parseCommand(command);
+	}
+}
---
+++ source/Bin/Minibloq/work/_examples/Multiplo/100.smallProtocol/videoJoystick.py
@@ -0,0 +1,137 @@
+''' Moves the robot according the captured movements (with the camera) of a blue object. '''
+
+## This software contains code derived from the work of Abid Rahman and Jay Edry.
+## The original work is available from: https://github.com/abidrahmank (specifically
+## from https://github.com/abidrahmank/MyRoughWork).
+## Please the license file here:
+## https://github.com/abidrahmank/MyRoughWork/blob/master/license.txt
+
+import sys
+import serial
+import cv2
+import numpy as np
+
+
+##Settings:
+gridWidth = 2
+gridColor = [0,255,0]
+
+zeroZoneX = 10
+zeroZoneY = 10
+
+minBlue = 100
+maxBlue = 120
+
+minRed = 100
+maxRed = 120
+
+
+##Robot functions:
+def sendCommand(command):
+    print command
+    #sp.flushInput()
+    sp.write(command + '\n')
+    #rx = ''
+    #while sp.inWaiting() != 0:
+    #    rx = rx + sp.read()
+    #print rx
+
+
+##Color tracking functions:
+def getThresholdedImg(hsv):
+    #yellow = cv2.inRange(hsv,np.array((20,100,100)),np.array((30,255,255)))
+    #blue = cv2.inRange(hsv,np.array((minBlue,100,100)),np.array((maxBlue,255,255)))
+    blue = cv2.inRange(hsv,np.array((minBlue, 100, 100)),np.array((maxBlue,255,255)))
+    #both = cv2.add(yellow,blue)
+    return blue
+
+
+##Main program:
+##sp = serial.Serial('COM69', 115200) ##KC21 BT module
+sp = serial.Serial('COM83', 9600) ##HC-07 BT module (Multiplo robots).
+##sp = serial.Serial('COM76', 38400) ##SparkFun's XBee module (RedBot)
+
+##Initializes video capturing:
+c = cv2.VideoCapture(0)
+width,height = c.get(3),c.get(4)
+print "Frame width and height: ", width, height
+
+##Main application cycle:
+while(1):
+    ##Captures the image:
+    _,f = c.read()
+
+    ##Process the image:
+    f = cv2.flip(f,1)
+    blur = cv2.medianBlur(f,5)
+
+    if f is not None:
+        hsv = cv2.cvtColor(f,cv2.COLOR_BGR2HSV)
+        both = getThresholdedImg(hsv)
+        erode = cv2.erode(both,None,iterations = 3)
+        dilate = cv2.dilate(erode,None,iterations = 10)
+        contours,hierarchy = cv2.findContours(dilate,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+
+        width, height = cv2.cv.GetSize(cv2.cv.fromarray(f))
+        centerX = width/2;
+        centerY = height/2;
+
+        for cnt in contours:
+            x,y,w,h = cv2.boundingRect(cnt)
+            cx,cy = x+w/2, y+h/2
+
+            ##Draw tracking rectangles:
+            #if 20 < hsv.item(cy,cx,0) < 30:
+            #    ##Yellow:
+            #    cv2.rectangle(f,(x,y),(x+w,y+h),[0,255,255],2)
+                
+            if minBlue < hsv.item(cy,cx,0) < maxBlue:
+                ##Blue:
+                cv2.rectangle(f,(x,y),(x+w,y+h),[255,0,0],2)
+                cv2.line(f,(x,cy),(x+w,cy),[0,0,0],2)
+                cv2.line(f,(cx,y),(cx,y+h),[0,0,0],2)
+
+                ##After this, cx and cy are the centers of the current tracking blue rectangle:
+        
+                ##Robot commands:
+                powerY = (height/2 - cy)*200/height ## =(height/2 - y)*100/(height/2)
+                powerX = (width/2 - cx)*200/width
+                if abs(powerY) > zeroZoneY: ##The ZeroZoneY determines if the robot is stopped.
+                    ##This is the splitted transform function between screen and motor power spaces:
+                    if powerX  > zeroZoneX: 
+                        command = 'motors ' + str(powerY) + ' ' + str(powerY+(powerX*powerY/100))
+                    elif powerX < -zeroZoneX:
+                        command = 'motors ' + str(powerY-(powerX*powerY/100)) + ' ' + str(powerY)
+                    else:
+                        command = 'motors ' + str(powerY) + ' ' + str(powerY)
+                else:
+                    command = 'motors'
+                sendCommand(command)
+
+        ##Zero zone x:
+        zeroZoneXScreen = zeroZoneX*(width/2)/100
+        cv2.line(f,(centerX-zeroZoneX,0),(centerX-zeroZoneX,height),[0,255,255], 1)
+        cv2.line(f,(centerX+zeroZoneX,0),(centerX+zeroZoneX,height),[0,255,255], 1)
+
+        ##Zero zone y:
+        zeroZoneYScreen = zeroZoneY*(height/2)/100
+        cv2.line(f,(0,centerY-zeroZoneYScreen),(width,centerY-zeroZoneYScreen),[0,255,255], 1)
+        cv2.line(f,(0,centerY+zeroZoneYScreen),(width,centerY+zeroZoneYScreen),[0,255,255], 1)
+
+        ##Grid:
+        cv2.line(f,(0,centerY),(width,centerY),gridColor, gridWidth)
+        cv2.line(f,(centerX,0),(centerX,height),gridColor, gridWidth)
+
+        cv2.imshow('img',f)
+
+    ##Press ESC to end the program:
+    if cv2.waitKey(25) == 27:
+        break
+
+##Releases OpenCV's resources:
+cv2.destroyAllWindows()
+c.release()
+
+##Releases robot's resources:
+sendCommand('stop')
+sp.close()
